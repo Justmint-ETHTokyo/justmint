2a1ea3063a8997f4ea0cbd5720a65079aed34af2
moonchaeyeon <sso07069@gmail.com>
2023-04-15 14:27:12 +0900

    [design] change entering passcode page view

--- client/src/pages/nftTransfer/EnterPassCode.tsx
+++ client/src/pages/nftTransfer/EnterPassCode.tsx
@@ -1,6 +1,6 @@
 import { chainType } from "ChainType";
 import { useGetPrivateKey } from "../../hook/useGetPrivateKey";
-import PassCodeInput from "../walletPrivateKey/PassCodeInput";
+import { setShowAlertInfo } from "../../utils/function/showAlert";
 
 type enterPassCodeProp = {
     nftChainType: chainType;
@@ -15,7 +15,8 @@ function EnterPassCode({nftChainType, makeSignature}:enterPassCodeProp) {
             const _privateKey = await getPrivateKey(nftChainType);
             return _privateKey;
         } catch(err) {
-            console.log(err);
+            setShowAlertInfo("비밀번호가 일치하지 않습니다.\n다시 입력해주세요.", false);
+            throw err;
         }
     }
 
@@ -29,14 +30,37 @@ function EnterPassCode({nftChainType, makeSignature}:enterPassCodeProp) {
     }
 
     return (
-        <div>
-            <PassCodeInput 
-                passCode={passCode}
-                setPassCode={setPassCode}
-                validPassCode={validPassCode}
-                submitAction={()=>{passCodeHandler()}}
-            />
-        </div>
+        <form 
+            className="transfer-sign"
+            onSubmit={(e)=>{e.preventDefault(); passCodeHandler()}}
+        >
+            <div className="transfer-sign-title">
+                transaction 실행을 위해서<br/>패스코드를 입력해주세요.
+            </div>
+            <div className="input-box-wrapper">
+                <input 
+                    className="input-textbox"
+                    type="password"
+                    placeholder="비밀번호를 입력해주세요."
+                    value={passCode}
+                    onChange={(e)=>{setPassCode(e.target.value)}}
+                />
+                {
+                    !validPassCode &&
+                    <div className="input-status" id="warning">
+                        올바르지 않은 비밀번호 입니다.
+                    </div>
+                }
+                <button
+                    className="transfer-sign-submit-button button"
+                    id="purple"
+                    disabled={!passCode.length}
+                >
+                    서명하기
+                </button>
+            </div>
+
+        </form>
     )
 }
 export default EnterPassCode;
